04b8002942c031070e6cfbebc313a66056de42e4
Aditya Maulana <aditmau26@gmail.com>
2022-02-22 15:18:32 +0700

    cokipardede

---
+++ src/main/java/enterprise001/enterprise001/Enterprise001Application.java
@@ -0,0 +1,13 @@
+package enterprise001.enterprise001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enterprise001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enterprise001Application.class, args);
+	}
+
+}
---
+++ src/test/java/enterprise001/enterprise001/Enterprise001ApplicationTests.java
@@ -0,0 +1,13 @@
+package enterprise001.enterprise001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enterprise001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
